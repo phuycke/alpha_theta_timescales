708ee5d0888de83eb3c380233df5c659b99a90d4
Pieter Huycke <24294920+phuycke@users.noreply.github.com>
2021-04-30 12:24:03 +0200

    Add working code for Figure 4 high DPI

--- 4. Plots/3. Figure 4/figure 4.py
+++ 4. Plots/3. Figure 4/figure 4.py
@@ -36,27 +36,27 @@ fig = plt.figure(figsize=(10, 9))
 gs  = gridspec.GridSpec(2, 13)
 
 # TFR plot
-fig_3a   = plt.subplot(gs[0, :8])
+fig_4a   = plt.subplot(gs[0, :8])
 
 # topoplot
-fig_3b   = plt.subplot(gs[0, 8:])
+fig_4b   = plt.subplot(gs[0, 8:])
 
 # alpha on the fast timescale
-fig_3c_l = plt.subplot(gs[1, 0:3])       # novel condition
-fig_3c_r = plt.subplot(gs[1, 3:6])       # repeating condition
+fig_4c_l = plt.subplot(gs[1, 0:3])       # novel condition
+fig_4c_r = plt.subplot(gs[1, 3:6])       # repeating condition
  
 # alpha on the slow timescale
-fig_3d_l = plt.subplot(gs[1, 7:10])      # novel condition
-fig_3d_r = plt.subplot(gs[1, 10:13])     # repeating condition
+fig_4d_l = plt.subplot(gs[1, 7:10])      # novel condition
+fig_4d_r = plt.subplot(gs[1, 10:13])     # repeating condition
 
 #%%
 
 """
-Figure 3A
+Figure 4A
 """
 
-# path to the result of the permutation data
-PERM_DATA = r"C:\Users\pieter\OneDrive - UGent\Projects\2019\overtraining - PILOT 3\figures\Publish\Data\Stimulus-locked\Repetition 1 vs. repetition 8"
+# set the path to the data, and load a fitting Epoch object
+PERM_DATA = r"C:\Users\pieter\OneDrive - UGent\Projects\2019\overtraining - PILOT 3\figures\Publish\Data\Stimulus-locked\Block 1 vs. block 8 (2)"
 TIME_DATA = r"C:\Users\pieter\OneDrive - UGent\Projects\2019\overtraining - PILOT 3\figures\TF\Group level\data"
 
 # define frequency bands (log spaced for setting the y-ticks later on)
@@ -64,18 +64,18 @@ FREQS = np.logspace(np.log10(4),
                     np.log10(30), 
                     15)
 
-# load the time data, and select everything between 0 and 1s
+# load the time data, and select everything between 0 and 750 ms
 times     = np.load(os.path.join(TIME_DATA, "stimulus_times.npy"))
-times     = times[np.where((times > 0) & (times <= 1))]
+times     = times[np.where((times > 0) & (times <= .75))]
 
 # the the difference between x[0] and x[1] for each value in times, and divide 
-# by 2 if len(times) is larger than 1s, else fix this at 0.0005
+# by 2 if len(times) is larger than 750 ms, else fix this at 0.0005
 time_diff = np.diff(times) / 2. if len(times) > 1 else [0.0005]
 
 # compute the limits of the time window (x-axis)
     # start:  first value of time (a bit larger than 0) - 0.00048828
     # middle: all values except the last + 0.00048828
-    # final:  last value of time (1) + 0.00048828
+    # final:  last value of time (.75) + 0.00048828
 time_lims = np.concatenate([[times[0] - time_diff[0]], times[:-1] +
                             time_diff, [times[-1] + time_diff[-1]]])
 
@@ -105,9 +105,9 @@ time_mesh, yval_mesh = np.meshgrid(time_lims, yval_lims)
 
 # load the permutation test result array + check dimensions of the data
 f_obs = np.load(os.path.join(PERM_DATA, "f_obs.npy"))
-assert f_obs.shape == (64, 15, 1024)
+assert f_obs.shape == (64, 15, 768)
 
-# 64: electrodes, 15: frequencies, 1024: time points
+# 64: electrodes, 15: frequencies, 768: time points
 # we average over electrodes to retain the frequency and time information
 f_obs_mean = np.mean(f_obs, axis = 0)
 
@@ -117,15 +117,15 @@ gauss = ndimage.filters.gaussian_filter(f_obs_mean,
                                         mode = 'constant')
 
 # create a pseudocolor plot
-fig_3a.pcolormesh(time_mesh, 
-              yval_mesh, 
-              gauss,
-              cmap    = "RdBu_r",
-              shading = "gouraud")
+fig_4a.pcolormesh(time_mesh, 
+                  yval_mesh, 
+                  gauss,
+                  cmap    = "RdBu_r",
+                  shading = "gouraud")
 
 # draw a contour around larger values
 # we draw the contour around values that are percentile 97.5 or larger
-fig_3a.contour(time_mesh, 
+fig_4a.contour(time_mesh, 
                yval_mesh, 
                gauss, 
                levels     = [np.percentile(gauss, 97.5)], 
@@ -135,14 +135,14 @@ fig_3a.contour(time_mesh,
 
 # set the y-axis parameters, note that the y-axis needs to be converted to 
 # log, and that a ticker needs to be called to set the y-axis ticks
-fig_3a.set_yscale('log')
-fig_3a.get_yaxis().set_major_formatter(ticker.ScalarFormatter())
-fig_3a.yaxis.set_minor_formatter(ticker.NullFormatter())
-fig_3a.yaxis.set_minor_locator(ticker.NullLocator())
+fig_4a.set_yscale('log')
+fig_4a.get_yaxis().set_major_formatter(ticker.ScalarFormatter())
+fig_4a.yaxis.set_minor_formatter(ticker.NullFormatter())
+fig_4a.yaxis.set_minor_locator(ticker.NullLocator())
 
 # once the ticks are set, we assign the values of FREQS to the ticks
 tick_vals = yvals[np.unique(np.linspace(0, len(yvals) - 1, 15).round().astype('int'))]
-fig_3a.set_yticks(tick_vals)
+fig_4a.set_yticks(tick_vals)
 
 # determine the y-ticks
 ticks_str = []
@@ -151,16 +151,17 @@ for t in tick_vals:
         ticks_str.append("{0:.2f}".format(t))
     else:
         ticks_str.append(" ")
-fig_3a.set_yticklabels(ticks_str)
+fig_4a.set_yticklabels(ticks_str)
 
-# set the x-axis parameters: every 100 ms a label is placed
-fig_3a.set_xticks(np.arange(0, 1.1, .25))
-fig_3a.set_xticklabels([str(int(label)) for label in np.arange(0, 1001, 250)])
+
+# set the x-axis parameters: every 50 ms a label is placed
+fig_4a.set_xticks(np.arange(0, .751, .25))
+fig_4a.set_xticklabels([str(int(label)) for label in np.arange(0, 751, 250)])
 
 # set the general title, and the titles of the x-axis and the y-axis
-fig_3a.set_xlabel('Time after stimulus (ms)')
-fig_3a.set_ylabel('Frequency (Hz)')
-fig_3a.set_title("Stimulus 1 vs. 8: permutation test TFR\nAlpha on the fast timescale (p = 0.001)")
+fig_4a.set_xlabel('Time after stimulus (ms)')
+fig_4a.set_ylabel('Frequency (Hz)')
+fig_4a.set_title("Block 1 vs. 8: permutation test TFR\nTheta on slow timescale (p = 0.04)")
 
 # load the cluster data, and keep only the significant clusters
 clust       = np.load(os.path.join(PERM_DATA, "clust.npy"), allow_pickle = True)
@@ -174,7 +175,7 @@ for c, p_val in zip(clust, clust_p_val):
 f_obs_plot_mean = np.nanmean(f_obs_plot, axis = 0)
 
 # create a 2D raster of the significant data (no plot) to use for the colorbar
-im = fig_3a.imshow(f_obs_plot_mean,
+im = fig_4a.imshow(f_obs_plot_mean,
                    extent        = [times[0], times[-1], 
                                     FREQS[0], FREQS[-1]],
                    aspect        = "auto", 
@@ -183,60 +184,48 @@ im = fig_3a.imshow(f_obs_plot_mean,
                    cmap          = "RdBu_r")
 
 # get the colorbar of the above 2D raster, and paste it on the existing TFR plot
-# note that this data is used to create the colorbar, and not the filtered data
-# since the values become smaller due to the filtering process. The plot reflects
-# the actual data, filtering is only done for visual appeal
-cbar = fig.colorbar(im, ax = fig_3a)
+cbar = fig.colorbar(im, ax = fig_4a)
 
 # set some colorbar parameters, such as the title, ticks and tick labels
 cbar.ax.set_title("F-statistic", 
                   fontdict = {"fontsize": TEXT_SIZE})
-cbar.ax.get_yaxis().set_ticks(np.arange(0, np.round(np.max(f_obs_plot_mean), 1) + 0.05, 4))
+cbar.ax.get_yaxis().set_ticks(np.arange(0, np.round(np.max(f_obs_plot_mean), 1) + 0.05, 2))
 cbar.ax.tick_params(labelsize = TEXT_SIZE - 3)
 
 # big fix: make sure that the 0 is shown on the x-axis of the final plot 
-fig_3a.set_xbound(0, 1)
+fig_4a.set_xbound(0, .75)
 
 #%%
 
 """
-Figure 3B
+Figure 4B
 """
 
 # Determines which part of the analysis to run + some plotting parameters
 STIM_LOCKED = True
 COMPUTE_TFR = False
-BAND        = [(8, 12, "Alpha")]
-TMIN, TMAX  = .65, .9  
-VMIN, VMAX  = 0.5, 4.5
-
-rcParams['font.family'] = 'Times New Roman'
-rcParams['font.size']   = 8
-
-# these are the subjects that had all 512 epochs recorded and stored safely
-full_epochs = ["sub-02", "sub-03", "sub-04", "sub-05", "sub-06", "sub-08",
-               "sub-10", "sub-12", "sub-13", "sub-15", "sub-16", "sub-17",
-               "sub-18", "sub-19", "sub-20", "sub-21", "sub-22", "sub-23",
-               "sub-25", "sub-26", "sub-27", "sub-28", "sub-29", "sub-30"]
+BAND        = [(4, 8, "Theta")]
+TMIN, TMAX  = 0, .4
+VMIN, VMAX  = -.85, .85
 
 # load the TFR data
-rep1 = mne.time_frequency.read_tfrs(r"C:\Users\pieter\Downloads\repetition 1 (24 subs)-tfr.h5")[0]
-rep8 = mne.time_frequency.read_tfrs(r"C:\Users\pieter\Downloads\repetition 8 (24 subs)-tfr.h5")[0]
-    
-# save rep8 in temp, dB transform
-temp = rep8
-temp._data = 10 * np.log10(rep8._data)
+block1 = mne.time_frequency.read_tfrs(r"C:\Users\pieter\Downloads\block 1 (24 subs)-tfr.h5")[0]
+block8 = mne.time_frequency.read_tfrs(r"C:\Users\pieter\Downloads\block 8 (24 subs)-tfr.h5")[0]
 
-# save rep1 in temp2, dB transform
-temp2 = rep1
-temp2._data = 10 * np.log10(rep1._data)
+# save block8 in temp, dB transform
+temp = block8
+temp._data = 10 * np.log10(block8._data)
+
+# save block1 in temp2, dB transform
+temp2 = block1
+temp2._data = 10 * np.log10(block1._data)
 
 temp._data -= temp2._data
     
-# check whether the difference does not equal rep_1 or rep_8
-assert np.all(temp._data != rep1._data)
-assert not np.sum(temp._data != rep8._data)
-    
+# check whether the difference does not equal block1 or block8
+assert np.all(temp._data != block1._data)
+assert not np.sum(temp._data != block8._data)
+
 # colorbar with log scaled labels
 def fmt_float(x, pos):
     return r'${0:.2f}$'.format(x)
@@ -262,16 +251,16 @@ avg_tfr.plot_topomap(tmin     = TMIN,
                      colorbar = True,
                      cbar_fmt = fmt_float,
                      sensors  = "ko",
-                     axes     = fig_3b,
+                     axes     = fig_4b,
                      title    = " ")
 
 # set a title which can be altered
-fig_3b.set_title(r"$\alpha$ topography", size = TEXT_SIZE)
+fig_4b.set_title(r"$\theta$ topography", size = TEXT_SIZE)
 
 #%%
 
 """
-Figure 3C
+Figure 4C
 """
 
 # where to find the data files
@@ -290,7 +279,7 @@ df.columns = ['Reaction time (ms)', 'RT_log', 'Accuracy', 'Accuracy_int',
               'Subject nr', 'Repetitions_overall', 'Repetition count', 
               'Block_overall', 'Block number', 'Condition', 'Trial_overall', 
               'Trial_block', 'Response', 'Stimulus_ID']
-x_title, y_title = "Repetition count", "Alpha power"
+x_title, y_title = "Repetition count", "Theta power"
 
 # Novel condition
 g = sns.regplot(x           = x_title, 
@@ -303,7 +292,7 @@ g = sns.regplot(x           = x_title,
                 scatter_kws = {"s":15}, 
                 line_kws    = {'lw': .75},
                 color       = "darkgrey",
-                ax          = fig_3c_l)
+                ax          = fig_4c_l)
 
 # Recurring condition
 g = sns.regplot(x           = x_title, 
@@ -316,35 +305,35 @@ g = sns.regplot(x           = x_title,
                 scatter_kws = {"s":15}, 
                 line_kws    = {'lw': .75},
                 color       = "black",
-                ax          = fig_3c_r)
+                ax          = fig_4c_r)
 
 # figure parameters (left figure)
-fig_3c_l.set_title(r"Novel condition", size = TEXT_SIZE)   
-fig_3c_l.set_ylim([-.5, -.1])  
-fig_3c_l.set_yticks(np.arange(-.5, -.09, .1))   
-fig_3c_l.set_xticks(np.arange(1, 9))
-fig_3c_l.set_xlim(0.5, 8.5)
-fig_3c_l.set_xlabel(r"Stimulus number")
-fig_3c_l.set_ylabel(r"$\alpha$ power")
+fig_4c_l.set_title(r"Novel condition", size = TEXT_SIZE)   
+fig_4c_l.set_ylim([-.1, .1])  
+fig_4c_l.set_yticks(np.arange(-.1, .11, .1))   
+fig_4c_l.set_xticks(np.arange(1, 9))
+fig_4c_l.set_xlim(0.5, 8.5)
+fig_4c_l.set_xlabel(r"Stimulus number")
+fig_4c_l.set_ylabel(r"$\theta$ power")
 
 # figure parameters (right figure)
-fig_3c_r.set_xlim(0.5, 8.5)
-fig_3c_r.set_xticks(np.arange(1, 9))
-fig_3c_r.set_ylim([-.5, -.1])  
-fig_3c_r.set_yticks(np.arange(-.5, -.09, .1))   
-fig_3c_r.set_yticklabels([])   
-fig_3c_r.set_title(r"Repeating condition", size = TEXT_SIZE)   
-fig_3c_r.set_xlabel(r"Stimulus number")
-fig_3c_r.set_ylabel(" ")
+fig_4c_r.set_title(r"Repeating condition", size = TEXT_SIZE)   
+fig_4c_r.set_ylim([-.1, .1])  
+fig_4c_r.set_yticks(np.arange(-.1, .11, .1))   
+fig_4c_r.set_yticklabels([])   
+fig_4c_r.set_xticks(np.arange(1, 9))
+fig_4c_r.set_xlim(0.5, 8.5)
+fig_4c_r.set_xlabel(r"Stimulus number")
+fig_4c_r.set_ylabel("")
 
 #%%
 
 """
-Figure 3D
+Figure 4D
 """
 
 # new variables
-x_title, y_title = "Block number", "Alpha power"
+x_title, y_title = "Block number", "Theta power"
 
 # Novel condition
 g = sns.regplot(x           = x_title, 
@@ -357,7 +346,7 @@ g = sns.regplot(x           = x_title,
                 scatter_kws = {"s":15}, 
                 line_kws    = {'lw': .75},
                 color       = "darkgrey",
-                ax          = fig_3d_l)
+                ax          = fig_4d_l)
 
 # Recurring condition
 g = sns.regplot(x           = x_title, 
@@ -370,26 +359,26 @@ g = sns.regplot(x           = x_title,
                 scatter_kws = {"s":15}, 
                 line_kws    = {'lw': .75},
                 color       = "black",
-                ax          = fig_3d_r)
+                ax          = fig_4d_r)
 
 # figure parameters (left figure)
-fig_3d_l.set_title(r"Novel condition", size = TEXT_SIZE)   
-fig_3d_l.set_ylim([-.5, -.1])  
-fig_3d_l.set_yticks(np.arange(-.5, -.09, .1))   
-fig_3d_l.set_xticks(np.arange(1, 9))
-fig_3d_l.set_xlim(0.5, 8.5)
-fig_3d_l.set_xlabel(r"Block number")
-fig_3d_l.set_ylabel(r"$\alpha$ power")
+fig_4d_l.set_title(r"Novel condition", size = TEXT_SIZE)   
+fig_4d_l.set_ylim([-.1, .1])  
+fig_4d_l.set_yticks(np.arange(-.1, .11, .1))   
+fig_4d_l.set_xticks(np.arange(1, 9))
+fig_4d_l.set_xlim(0.5, 8.5)
+fig_4d_l.set_xlabel(r"Block number")
+fig_4d_l.set_ylabel(r"$\theta$ power")
 
-# figure parameters (right figure) 
-fig_3d_r.set_xlim(0.5, 8.5)
-fig_3d_r.set_xticks(np.arange(1, 9))
-fig_3d_r.set_ylim([-.5, -.1])  
-fig_3d_r.set_yticks(np.arange(-.5, -.09, .1))   
-fig_3d_r.set_yticklabels([]) 
-fig_3d_r.set_title(r"Repeating condition", size = TEXT_SIZE)   
-fig_3d_r.set_xlabel(r"Block number")
-fig_3d_r.set_ylabel(" ")
+# figure parameters (right figure)
+fig_4d_r.set_title(r"Repeating condition", size = TEXT_SIZE)   
+fig_4d_r.set_ylim([-.1, .1])  
+fig_4d_r.set_yticks(np.arange(-.1, .11, .1))   
+fig_4d_r.set_yticklabels([])   
+fig_4d_r.set_xticks(np.arange(1, 9))
+fig_4d_r.set_xlim(0.5, 8.5)
+fig_4d_r.set_xlabel(r"Block number")
+fig_4d_r.set_ylabel("")
 
 #%%
 
@@ -401,25 +390,25 @@ Save figure
 FIG = r"C:\Users\pieter\OneDrive - UGent\Projects\2019\overtraining - PILOT 3\figures\Publish\Correct DPI plots"
 
 # play around until the figure is satisfactory (difficult with high DPI)
-plt.subplots_adjust(top=0.932, bottom=0.077, left=0.097, right=0.938, 
-                    hspace=0.5, wspace=0.35)
+plt.subplots_adjust(top=0.932, bottom=0.077, left=0.097, right=0.933, 
+                    hspace=0.5, wspace=0.375)
 
 # letters indicating the panels
-plt.text(-245, 5, "A", size = TEXT_SIZE+5)    
-plt.text(-85, 5, "B", size = TEXT_SIZE+5)    
-plt.text(-245, -1, "C", size = TEXT_SIZE+5)    
-plt.text(-115, -1, "D", size = TEXT_SIZE+5)    
+plt.text(-105, 1.1, "A", size = TEXT_SIZE+5)    
+plt.text(-37, 1.1, "B", size = TEXT_SIZE+5)    
+plt.text(-105, -1.55, "C", size = TEXT_SIZE+5)    
+plt.text(-49, -1.55, "D", size = TEXT_SIZE+5)    
 
 # dB label for panel B
-plt.text(-1.5, 4.6, "dB", size = TEXT_SIZE)    
+plt.text(-1, .9, "dB", size = TEXT_SIZE)    
 
 # titles for panels C and D
-plt.text(-200, -1.15, r"$\alpha$ power ~ fast timescale", size = TEXT_SIZE)
-plt.text(-75, -1.15, r"$\alpha$ power ~ slow timescale", size = TEXT_SIZE)
+plt.text(-85, -1.55, r"$\theta$ power ~ fast timescale", size = TEXT_SIZE)
+plt.text(-35, -1.55, r"$\theta$ power ~ slow timescale", size = TEXT_SIZE)
 
 # save as tiff and pdf
-plt.savefig(fname = os.path.join(FIG, "Figure 3.tiff"), dpi = 300)
-plt.savefig(fname = os.path.join(FIG, "Figure 3.pdf"), dpi = 300)
+plt.savefig(fname = os.path.join(FIG, "Figure 4.tiff"), dpi = 300)
+plt.savefig(fname = os.path.join(FIG, "Figure 4.pdf"), dpi = 300)
 
 plt.close("all")
 
